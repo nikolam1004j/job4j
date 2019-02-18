ec8ee2bcebd7dbd592748cb84f9b45390fe0f872
Nikolay Razilov <nickraz@yandex.ru>
2019-02-18 23:09:22 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculate;
-
+/**
+* Calculate class for calculating
+*
+* @author Nikolay Razilov
+* @since 18.02.2019
+* @version 1
+*/
 class Calculate{
 	public static void main(String [] args){
 		System.out.println("Hello world");
