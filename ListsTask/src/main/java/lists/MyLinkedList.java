e4259dc84f06aa4e1104ff25ed8614c1465e3493
Nikolay Razilov <nickraz@yandex.ru>
2019-02-21 14:09:10 +0300

    5.3.2. Создать контейнер на базе связанного списка [#115215]

---
+++ ListsTask/src/main/java/lists/MyLinkedList.java
@@ -0,0 +1,146 @@
+package lists;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Контейнер на базе связанного списка.
+ * @param <E> Тип данных.
+ */
+public class MyLinkedList<E> implements Iterable<E> {
+
+    private Node<E> first;
+    private int size;
+    private int modCount;
+
+    /**
+     * Возвращает размер списка.
+     *
+     * @return Размер списка.
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Добавляет элемент в список.
+     *
+     * @param val Добавляемый элемент.
+     */
+    public void add(E val) {
+        if (first == null) {
+            first = new Node<>(val);
+        } else {
+            Node<E> node = first;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<>(val);
+            node.next.prev = node;
+        }
+        modCount++;
+        size++;
+    }
+
+    /**
+     * Возвращает узел по индексу.
+     *
+     * @param index Индекс.
+     * @return Узел по индексу.
+     */
+    private Node<E> getNode(int index) {
+        if (index >= size || size == 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    /**
+     * Возвращает значение узла по индексу.
+     *
+     * @param index Индекс.
+     * @return Значение узла.
+     */
+    public E get(int index) {
+        Node<E> node = getNode(index);
+        return node.date;
+    }
+
+    /**
+     * Удаляет узел по индексу, возвращая его значение.
+     *
+     * @param index Индекс.
+     * @return Возвращаемое значение удаляемого узла.
+     */
+    public E remove(int index) {
+        Node<E> node = getNode(index);
+        E result = node.date;
+        if (node == first) {
+            if (node.next == null) {
+                node = null;
+            } else {
+                node.next.prev = null;
+                first = first.next;
+            }
+        } else if (index == size - 1) {
+            node.prev.next = null;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+        modCount++;
+        size--;
+        return node == null ? result : node.date;
+    }
+
+    /**
+     * Возвращает итератор для списка.
+     *
+     * @return Итератор для списка.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> curNode = first;
+            private int innerModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (innerModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return curNode != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E result = curNode.date;
+                curNode = curNode.next;
+                return result;
+            }
+        };
+    }
+
+    /**
+     * Класс узла.
+     *
+     * @param <E> Тип данных.
+     */
+    private static class Node<E> {
+        private E date;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(E date) {
+            this.date = date;
+        }
+    }
+}
---
+++ ListsTask/src/test/java/lists/MyLinkedListTest.java
@@ -0,0 +1,107 @@
+package lists;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MyLinkedListTest {
+
+    @Test
+    public void simpleTest() {
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        myLinkedList.add(5);
+
+        assertThat(myLinkedList.get(0), is(1));
+        assertThat(myLinkedList.get(1), is(2));
+        assertThat(myLinkedList.get(2), is(3));
+        assertThat(myLinkedList.getSize(), is(5));
+        assertThat(myLinkedList.remove(0), is(1));
+        assertThat(myLinkedList.getSize(), is(4));
+        assertThat(myLinkedList.get(0), is(2));
+        assertThat(myLinkedList.get(myLinkedList.getSize() - 1), is(5));
+    }
+
+    @Test
+    public void iteratorTest() {
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        myLinkedList.add(5);
+
+        assertThat(myLinkedList.remove(1), is(2));
+        assertThat(myLinkedList.remove(2), is(4));
+        assertThat(myLinkedList.getSize(), is(3));
+
+        Iterator<Integer> iterator = myLinkedList.iterator();
+        StringBuilder sb = new StringBuilder();
+        while (iterator.hasNext()) {
+            sb.append(iterator.next());
+        }
+        assertThat(sb.toString(), is("135"));
+    }
+
+    @Test
+    public void iteratorTestNextAndHasNext() {
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        myLinkedList.add(5);
+
+        Iterator<Integer> iterator = myLinkedList.iterator();
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.next(), is(2));
+        assertThat(iterator.hasNext(), is(true));
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void concurrentModificationExceptionAfterAddItemTest() {
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        myLinkedList.add(5);
+
+        Iterator<Integer> iterator = myLinkedList.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        myLinkedList.add(6);
+        iterator.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void concurrentModificationExceptionAfterDeleteItemTest() {
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        myLinkedList.add(5);
+
+        Iterator<Integer> iterator = myLinkedList.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        myLinkedList.remove(myLinkedList.getSize() - 1);
+        iterator.next();
+    }
+}
