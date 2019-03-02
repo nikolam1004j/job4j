90782342cc2c18532de22f65a3a1de0a7255c914
Nikolay Razilov <nickraz@yandex.ru>
2019-03-02 17:10:23 +0300

    2. Удаление запрещенных слов[#115252]

--- Part2InputOutput/IIOO/src/main/java/iopack/ChekingByteArray.java
+++ Part2InputOutput/IIOO/src/main/java/iopack/ChekingByteArray.java
@@ -1,6 +1,8 @@
 package iopack;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Scanner;
 
 public class ChekingByteArray {
@@ -12,5 +14,20 @@ public class ChekingByteArray {
             }
         }
         return result;
+    }
+
+    void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
+        byte[] lineSeparator = System.getProperty("line.separator").getBytes();
+        try(Scanner sc = new Scanner(in)) {
+            while (sc.hasNext()) {
+                String curLine = sc.nextLine();
+                for (String badWord : abuse) {
+                    curLine = curLine.replace(badWord, "");
+                }
+                out.write(curLine.getBytes());
+                out.write(lineSeparator);
+            }
+            out.flush();
+        }
     }
 }
--- Part2InputOutput/IIOO/src/test/java/iopack/ChekingByteArrayTest.java
+++ Part2InputOutput/IIOO/src/test/java/iopack/ChekingByteArrayTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class ChekingByteArrayTest {
 
@@ -15,4 +16,45 @@ public class ChekingByteArrayTest {
         }
     }
 
+    @Test
+    public void testBadWords() throws IOException {
+        ChekingByteArray chekingByteArray = new ChekingByteArray();
+        String input = "src\\main\\resources\\input.txt";
+        String output = "src\\main\\resources\\output.txt";
+        String[] badWords = {"Lorem", "Ipsum", "with", "Vestibulum"};
+        try(InputStream inputStream = new FileInputStream(input)) {
+            try(OutputStream outputStream = new FileOutputStream(output)) {
+                chekingByteArray.dropAbuses(inputStream,
+                        outputStream,
+                        badWords);
+            }
+        }
+
+        //После выполнения процедуры dropAbuses заново загружаем файлы
+        String sep = System.getProperty("line.separator");
+        StringBuilder sbFileInput = new StringBuilder();
+        try(FileReader fr = new FileReader(input)) {
+            Scanner sc = new Scanner(fr);
+            while (sc.hasNext()) {
+                sbFileInput.append(sc.nextLine()).append(sep);
+            }
+        }
+
+        StringBuilder sbFileOutput = new StringBuilder();
+        try(FileReader fr = new FileReader(output)) {
+            Scanner sc = new Scanner(fr);
+            while (sc.hasNext()) {
+                sbFileOutput.append(sc.nextLine()).append(sep);
+            }
+        }
+
+        //Циклом заменяем все плохие слова и сравниваем с выходным файлом,
+        //который записала процедура dropAbuses.
+        String afterReplaceString = sbFileInput.toString();
+        for (String badWord : badWords) {
+            afterReplaceString = afterReplaceString.replace(badWord, "");
+        }
+
+        Assert.assertEquals(afterReplaceString, sbFileOutput.toString());
+    }
 }
