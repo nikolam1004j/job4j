182f63d34f354aaef268b6e78a4061c759f1cadc
Nikolay Razilov <nickraz@yandex.ru>
2019-03-12 23:58:51 +0300

    1. Пин-понг.[#118638]

---
+++ Middle/src/main/java/threads/RectangleMove.java
@@ -0,0 +1,38 @@
+package threads;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+    private final int limitX;
+    private final int limitY;
+
+    public RectangleMove(Rectangle rect, int limitX, int limitY) {
+        this.rect = rect;
+        this.limitX = limitX;
+        this.limitY = limitY;
+    }
+
+    @Override
+    public void run() {
+        int koefX = 1;
+        int koefY = 1;
+        while (true) {
+            if ((rect.getX() == limitX - rect.getWidth() && koefX > 0) ||
+                    (rect.getX() == 0 && koefX < 0)) {
+                koefX = -koefX;
+            }
+            if ((rect.getY() == limitY - rect.getHeight() && koefY > 0) ||
+                    (rect.getY() == 0 && koefY < 0)) {
+                koefY = -koefY;
+            }
+            this.rect.setX(this.rect.getX() + koefX);
+            this.rect.setY(this.rect.getY() + koefY);
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
