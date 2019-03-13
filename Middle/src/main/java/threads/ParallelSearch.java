2bd00f90d44e643bc63dcc0ec4934e0f0cfdfd7c
Nikolay Razilov <nickraz@yandex.ru>
2019-03-13 16:36:12 +0300

    2. Обеспечить остановку потребителя.[#118633]

---
+++ Middle/src/main/java/threads/ParallelSearch.java
@@ -0,0 +1,38 @@
+package threads;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+
+        final Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+
+        );
+        producer.start();
+    }
+}
